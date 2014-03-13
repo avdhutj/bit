07edaa2f2aba19ef009bd3a9a0ce64c0c42bdf3c
avdhutj <avdhutj@gmail.com>
2014-03-13 19:16:58 -0400

    Saving scratch

--- scratch.py
+++ scratch.py
@@ -1,13 +1,19 @@
+#!/usr/bin/python
 from bitfinex import Bitfinex
 from btce import BTCE
 from bot import Bot
+
+import btcelogin
 
 bfinex = Bitfinex()
 btce = BTCE()
 
 bot = Bot()
-bot.addExchange(bfinex)
 bot.addExchange(btce)
+bot.addExchange(bfinex)
+
 
 print 'Running Bot'
 bot.run()
+
+#btcelogin.btce_to_bitfinex(0, 'haha')
